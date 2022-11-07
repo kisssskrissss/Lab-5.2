a552135cc5f2ac9909de55bc0738d37d4f2fc9fe
krist <krist@LAPTOP-HIQLHLL6>
2022-11-07 20:46:21 +0500

    Добавьте файлы проекта.

---
+++ Lab 5.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0; int min = 50;
+            int[] array = new int[15];
+            Random random = new Random();
+            for (int i = 0; i < 15; i++)
+            {
+                array[i] = random.Next(0, 50);
+                Console.WriteLine(array[i]);
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+            }
+            Console.WriteLine("min = {0}", min);
+            Console.WriteLine("max = {0}", max);
+            Console.ReadKey();
+        }
+    }
+}
